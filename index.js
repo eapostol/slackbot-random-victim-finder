ef866ce65d3f89327124221758bd45132792fb4e
Mix Master Mike <mikeyamato@gmail.com>
2018-09-16 21:49:21 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+const monWed = require('./mon-wed');
+const tueThu = require('./tue-thu');
+
+const sat = monWed.concat(tueThu);
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send(
+		'Hello There'
+	);
+});
+
+app.get('/mw', (req, res) => {
+	res.send(
+		monWed[Math.floor(Math.random() * monWed.length)]
+	);
+});
+
+app.get('/tth', (req, res) => {
+	res.send(
+		tueThu[Math.floor(Math.random() * tueThu.length)]
+	);
+});
+
+app.get('/sat', (req, res) => {
+	res.send(
+		// console.log(sat.length)
+		// console.log(sat)
+		console.log(sat[Math.floor(Math.random() * sat.length)])
+		// sat[Math.floor(Math.random() * sat.length)]
+	);
+});
+
+app.listen(port, () => console.log(
+	`Server Running on Port ${port}`
+));
---
+++ mon-wed.js
@@ -0,0 +1,23 @@
+module.exports = [
+	"Angel Aquas",
+	"Beverly Reyes",
+	"Bryan Pacana",
+	"Bryan Tran",
+	"Freddie Talai",
+	"Garry Maghari",
+	"German Pineda",
+	"Hadi Burpee",
+	"Jacob Adler",
+	"Jobi Mathai",
+	"Joseph Clarke",
+	"Louis McGruder",
+	"Lynn Thompson",
+	"Margaret He",
+	"Nicholas Antonyan",
+	"Olga Vyrvich",
+	"Oswaldo Medina",
+	"Rajiv Jagirdar",
+	"Rashid Green",
+	"Vaibhav Pandey",
+	"William Collins"
+];
