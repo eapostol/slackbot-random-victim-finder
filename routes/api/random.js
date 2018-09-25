cd5d94c6b2f1e04af1c2c80079103972fb3c8e65
Mix Master Mike <mikeyamato@gmail.com>
2018-09-24 18:31:27 -0700

    updated random.js

--- routes/api/random.js
+++ routes/api/random.js
@@ -74,6 +74,9 @@ router.post('/', (req, res) => {
 		// console.log('**** 6', secondRequest.callback_id);
 
 		if(secondRequest.callback_id === 'hunt_victim_mw'){
+			previouslySelected();
+			mwVictim = tempArr[tempArr.length-1];
+
 			return res.status(200).send(
 				{
 					"text": `_*${mwVictim}*_${luckyMsg} \n${byeMsg} \n${emoji}`,
